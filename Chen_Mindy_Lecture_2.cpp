dcb1c27f6d406397572ec4ce46c7ce6637225fae
Mindyc123 <MIndyc14@nycstudents.net>
2024-09-16 16:33:57 -0400

    Create Chen_Mindy_Lecture_2.cpp

---
+++ Chen_Mindy_Lecture_2.cpp
@@ -0,0 +1,21 @@
+//Mindy Chen
+//Lecture 2
+//9.16.24
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+int two = 2;
+float dec = 3.5;
+double decs = 3.9;
+char care = 'g';
+bool ft = true;
+string li = "Yes";
+
+cout << 
+
+    return 0;
+}
